8e642fe16238a3a8d9c1ac3620661635ab67d6db
Ivan Franchin <ivan.franchin@takeaway.com>
2023-03-11 09:25:29 +0100

    remove mapstruct to keep the project simpler

---
+++ movie-api/src/main/java/com/ivanfranchin/movieapi/mapper/MovieMapperImpl.java
@@ -0,0 +1,22 @@
+package com.ivanfranchin.movieapi.mapper;
+
+import com.ivanfranchin.movieapi.model.Movie;
+import com.ivanfranchin.movieapi.rest.dto.CreateMovieRequest;
+import com.ivanfranchin.movieapi.rest.dto.MovieDto;
+import org.springframework.stereotype.Service;
+
+import java.time.format.DateTimeFormatter;
+
+@Service
+public class MovieMapperImpl implements MovieMapper {
+
+    @Override
+    public Movie toMovie(CreateMovieRequest createMovieRequest) {
+        return new Movie(createMovieRequest.getImdb(), createMovieRequest.getTitle(), createMovieRequest.getPoster());
+    }
+
+    @Override
+    public MovieDto toMovieDto(Movie movie) {
+        return new MovieDto(movie.getImdb(), movie.getTitle(), movie.getPoster(), DateTimeFormatter.ISO_OFFSET_DATE_TIME.format(movie.getCreatedAt()));
+    }
+}
---
+++ movie-api/src/main/java/com/ivanfranchin/movieapi/mapper/UserMapperImpl.java
@@ -0,0 +1,14 @@
+package com.ivanfranchin.movieapi.mapper;
+
+import com.ivanfranchin.movieapi.model.User;
+import com.ivanfranchin.movieapi.rest.dto.UserDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public UserDto toUserDto(User user) {
+        return new UserDto(user.getId(), user.getUsername(), user.getName(), user.getEmail(), user.getRole());
+    }
+}
